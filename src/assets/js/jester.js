6735bdc0e66146b2746bb34d7875a6fee1a8bca1
chrisciampoli <chrisciampoli@gmail.com>
2016-03-13 21:11:44 -0700

    Updates

--- src/assets/js/jester.js
+++ src/assets/js/jester.js
@@ -2,15 +2,36 @@ var ContainerBuilder = require('crate-js').ContainerBuilder,
     ConfigService    = require('./services/ConfigService'),
     LoginService     = require('./services/LoginService'),
     EventEmitter     = require('events').EventEmitter,
-    container        = ContainerBuilder.buildFromJson({
-        parameters: {
-            siteName: "Jester",
-        },
-        services: {
-            dispatcher: {module: EventEmitter},
-            configService: {module: ConfigService, args: ['%siteName%']},
-            loginService: {module: LoginService, args: ['@configService']}
-        }
-    });
+    Repository       = require('./utils/Repository');
 
-window.jester = container;
+
+export default class Jester {
+  constructor() {
+
+  }
+
+  run() {
+    this.modules   = this.buildModules();
+    this.container = ContainerBuilder.buildFromJson({
+          parameters: {
+              siteName: "Jester",
+          },
+          services: {
+              dispatcher: {module: EventEmitter},
+              configService: {module: ConfigService, args: ['%siteName%']},
+              loginService: {module: LoginService, args: ['@configService']}
+          }
+      });
+  }
+
+  buildModules() {
+    let repository = new Repository();
+  }
+
+  getModule(name) {
+    return this.modules.fetch(name);
+  }
+}
+
+window.jester = new Jester();
+jester.run();
